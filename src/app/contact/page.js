faf0bae92a95604ec1305968c9be9235c6a964b7
Kushagra Sachdeva <sachdevak806@gmail.com>
2024-12-04 14:25:00 +0530

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -49,6 +49,10 @@ const Contact = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
+      <br />
+      <br />
+      <br />
+      <br />
       <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div>
