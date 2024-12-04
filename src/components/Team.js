56bcc6f08f896a43caa5ced3f7e848e8b690e73c
Kushagra Sachdeva <sachdevak806@gmail.com>
2024-12-04 15:01:51 +0530

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,23 +2,22 @@ import { FaFacebook, FaInstagram } from 'react-icons/fa';
 
 const Team = () => {
   return (
-    <section className="container mx-auto px-5 md:px-16 lg:px-24 relative"> {/* Add relative positioning here */}
+    <section className="container mx-auto px-5 md:px-16 lg:px-24 relative">
       <span className="service-name text-center">OUR FOUNDER</span>
       <h2 className="title text-center md:w-1/2 mx-auto">
-      “Visionary founder leading a team of excellence.""
+        “Visionary founder leading a team of excellence.”
       </h2>
 
       <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 sm:gap-8 mt-16 relative">
-        <div className="absolute left-[475px] top-0 flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center mx-auto">
           {/* Team Card */}
           <img
             src="/team/img-1.jpeg"
             alt="Founder"
-            className="w-40 h-40 rounded-full object-cover mb-4"
+            className="w-40 h-40 rounded-full object-cover mb-4 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64" // Responsive image sizes
           />
           <h3 className="text-xl font-semibold text-center">Dhiraj Sachdeva</h3>
           <p className="text-sm text-gray-500 text-center">Founder</p>
-          
 
           {/* Social Media Icons */}
           <div className="mt-4 flex space-x-4 justify-center">
